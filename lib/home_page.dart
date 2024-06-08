6fd6e74b87685bcd9c1d7fc4fd6dab9f063bcef7
Kamil Gadek <kamil.gadek124@gmail.com>
2024-06-08 17:21:27 +0200

    Make ui of second card

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -137,7 +137,9 @@ class _HomePageState extends State<HomePage> {
                         color: color.AppColor.homePageContainerTextSmall,
                       ),
                     ),
-                    SizedBox(height: 25,),
+                    const SizedBox(
+                      height: 25,
+                    ),
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
@@ -162,26 +164,109 @@ class _HomePageState extends State<HomePage> {
                           ],
                         ),
                         Expanded(child: Container()),
-                         Container(
+                        Container(
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(60),
-                            boxShadow: [
-                              BoxShadow(color: color.AppColor.gradientFirst,
-                              blurRadius: 10,
-                              offset: Offset(4, 8),
-                              ),
-                            ]
-                          ),
-                           child: Icon(
+                              borderRadius: BorderRadius.circular(60),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: color.AppColor.gradientFirst,
+                                  blurRadius: 10,
+                                  offset: const Offset(4, 8),
+                                ),
+                              ]),
+                          child: const Icon(
                             Icons.play_circle_fill,
                             color: Colors.white,
                             size: 60,
-                                                   ),
-                         ),
+                          ),
+                        ),
                       ],
                     ),
                   ],
                 ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Container(
+              height: 180,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(top: 30),
+                    height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                          image: AssetImage(
+                            'assets/card.png',
+                          ),
+                          fit: BoxFit.fill,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 40,
+                            offset: const Offset(8, 10),
+                            color:
+                                color.AppColor.gradientSecond.withOpacity(0.3),
+                          ),
+                          BoxShadow(
+                            blurRadius: 10,
+                            offset: const Offset(-1, -5),
+                            color:
+                                color.AppColor.gradientSecond.withOpacity(0.3),
+                          )
+                        ]),
+                  ),
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(right: 200, bottom: 30),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
+                        image: AssetImage(
+                          'assets/figure.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 100,
+                    margin: const EdgeInsets.only(
+                      top: 50,
+                      left: 150,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'You are doing great',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: color.AppColor.homePageDetail,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        RichText(
+                          text: TextSpan(
+                            text: 'Keep it up\n',
+                            style: TextStyle(
+                              color: color.AppColor.homePagePlanColor,
+                              fontSize: 16,
+                            ),
+                            children: const [
+                              TextSpan(text: 'Stick to your plany'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
